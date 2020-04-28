6784cea47abdff15e6d5b5d985adf076c02c9942
Spazcool <douglas.wrightiii@gmail.com>
2020-04-27 22:11:01 -0400

    AC met, addint extras

--- script.js
+++ script.js
@@ -1,56 +1,75 @@
 // TODO AC
     // X show current date in header
-    // time blocks for standard workday
-    //     time blocks are colore coded
+    // X time blocks for standard workday
+    // X    time blocks are colore coded
     //         past
     //         present
     //         future
-    //     time block has an input field
+    // X    time block has an input field
     //         save event to that time in local stoarge
 
 $(function(){
     // X show current date in header
     $("#currentDay").text(moment().format('LL'));
-
+    let currentHour = moment().hour();
+    let colorCode = 'red';
     // time blocks for standard workday
     for(let i = 0; i < 24; i++){
-        $("#timeBlocks").append(`<div class='row' style='border: 2px solid purple;'>
-        <div class='col-md-10' style='border: 2px solid yellow;'>
-        <span>Hour: ${i}</span><input>Enter thing here</input>
-        </div>
-        <div class='col-md-1' style='border: 2px solid green;'>
-        <button>Create</button>
-        </div>
-        <div class='col-md-1' style='border: 2px solid black;'><button>Delete</button>
-        </div>
-        </div>`);
+        if(i === currentHour){
+            colorCode = 'yellow';
+        }
+        if(i > currentHour){
+            colorCode = 'green'
+        }
+        $("#timeBlocks").append(
+        `<form class='form-inline m-2 p-2' style='background-color:${colorCode};'>
+            <label for='inlineFormInputName2' class='m-2'>Hour: ${i < 10 ? '0' + i : i}</label>
+            <input type='text' class='form-control mr-sm-2' data-hour='${i}' placeholder='Eat, sleep, code' minlength='2' maxlength='50'></input>
+            <button type='button' class='btn btn-primary create mr-sm-2' data-hour='${i}'>Create</button>
+            <button type='button' class='btn btn-success delete mr-sm-2' data-hour='${i}'>Delete</button>
+            <p class='m-sm-2' data-hour='${i}' style='background-color: white;'>NICE TO HAVE: LIST OF CURRENT HELD VALUE</p>
+        </form>`
+        );
+
     }
 
+    $(".create").on("click", function() {
+        let saved = (localStorage.getItem("savedActivities")) ? JSON.parse(localStorage.getItem("savedActivities")) : [];
+        let buttonVal = $(this).attr("data-hour");
+        // LOOP THROUGH THE INPUT FIELDS
+        $('input[data-hour]').each(function(){
+            if($(this).attr("data-hour") == buttonVal){
+                // IF THERE ISN'T A PREVIOUS LOCAL SAVE CREATE ONE WITH THE RIGHT DATA STRUCTURE
+                if(!saved.length){
+                    saved.push({"time": $(this).attr("data-hour"), "activity" : [$(this).val()]});
+                }else{
+                    saved.forEach(hour => {
+                        // IF THERE IS A PREVIOUS LOCAL SAVE AT THIS SPOT, ADD THE NEW STRING TO IT
+                        if(hour.time == buttonVal){
+                            hour.activity.push($(this).val());
+                        }else{
+                            // IF THERE ISN'T A PREVIOUS LOCAL SAVE AT THIS SPOT CREATE ONE WITH THE RIGHT DATA STRUCTURE
+                            saved.push({"time": $(this).attr("data-hour"), "activity" : [$(this).val()]});
+                        }                 
+                    })
+                }
+                localStorage.setItem("savedActivities", JSON.stringify(saved));
+                
+                // $("p[data-hour]").each(function(){
+                //     if($(this).attr("data-hour") == buttonVal){
+                //         $(this).text('').text(saved);
+
+                //     }
+                // })
+            }
+        console.log('end saved: ', saved);
+    })
 })
-
-
-{/* 
-<div id="timeBlocks" style="border: 2px solid blue;">
-
-<div class='row' style='border: 2px solid purple;'>
-  <div class='col-md-10' style='border: 2px solid yellow;'>
-    <input>Enter thing here</input>
-  </div>
-  <div class='col-md-1' style='border: 2px solid green;'>
-    <button>Create</button>
-  </div>
-  <div class='col-md-1' style='border: 2px solid black;'>
-    <button>Delete</button>
-  </div>
-</div>
-
-*/}
-
-
 
 // TODO Nice to have
     // select any day
     // delete button on an timeblock
+    // PUSH CURRENT VALS TO A P
 
 $("#dayPicked").text(moment().day());
 $("#dayEarlier").on("click", () => {
@@ -66,4 +85,5 @@ $("#dayLater").on("click", () => {
     console.log('add ', parseInt(currentChosen))
     console.log('add ', (parseInt(currentChosen) + 1) % 6)
     $("#dayPicked").text(moment().add((parseInt(currentChosen) + 2), 'day').weekday())
+})
 })
